75adf39fb2213ed30de21f4f8c55741375697580
tameikareed <tameika.reed@tn.gov>
2021-03-23 12:18:49 -0400

    added changes

--- ssm_linux_agent.sh
+++ ssm_linux_agent.sh
@@ -2,19 +2,22 @@
 
 set -x 
 
-DIR="/boot"
+#DIR="/boot"
+DIR="/tmp"
+SSM_RPM="https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm"
 
-echo "Creating directory called post_launch in boot"
+echo "Checking for directory post_launch in boot"
 if [ -f  "$DIR/post_launch" ]
 then
       echo "We have success the dir is there"	
 fi
 
 echo "Downloading ssm-agent rpm"
-sudo wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm -P  $DIR/post_launch 
+sudo wget $SSM_RPM  -P  $DIR/post_launch 
 
 echo "Do a yum local install of the agent"
-sudo yum -y localinstall $DIR/post_launch/amazon-ssm-agent.rpm
+#sudo yum -y localinstall $DIR/post_launch/amazon-ssm-agent.rpm
+echo "sudo yum -y localinstall $DIR/post_launch/amazon-ssm-agent.rpm"
 
 
 echo "Enalbe the service for SSM agent"
