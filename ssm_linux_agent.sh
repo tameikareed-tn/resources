08467244b7d01743434d7931a2b2b2b129e764c6
tameikareed <tameika.reed@tn.gov>
2021-03-24 15:47:36 -0400

    removed comments

--- ssm_linux_agent.sh
+++ ssm_linux_agent.sh
@@ -2,8 +2,7 @@
 
 set -x 
 
-#DIR="/boot"
-DIR="/tmp"
+DIR="/boot"
 SSM_RPM="https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm"
 
 echo "Checking for directory post_launch in boot"
@@ -16,19 +15,15 @@ echo "Downloading ssm-agent rpm"
 sudo wget $SSM_RPM  -P  $DIR/post_launch 
 
 echo "Do a yum local install of the agent"
-#sudo yum -y localinstall $DIR/post_launch/amazon-ssm-agent.rpm
-echo "sudo yum -y localinstall $DIR/post_launch/amazon-ssm-agent.rpm"
+sudo yum -y localinstall $DIR/post_launch/amazon-ssm-agent.rpm
 
 
 echo "Enalbe the service for SSM agent"
-#sudo systemctl enable amazon-ssm-agent
-echo "sudo systemctl enable amazon-ssm-agent"
+sudo systemctl enable amazon-ssm-agent
 
 
 echo "Start the agent"
-#sudo systemctl start amazon-ssm-agent
-echo "sudo systemctl start amazon-ssm-agent"
+sudo systemctl start amazon-ssm-agent
 
 echo "Confirm status of agent running"
-#sudo systemctl status amazon-ssm-agent
-echo "sudo systemctl status amazon-ssm-agent"
+sudo systemctl status amazon-ssm-agent
